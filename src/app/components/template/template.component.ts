4b9f9787a1238ae2be7ada808727b73d8b2f10e5
beremonth <beremonth@hotmail.com>
2019-12-27 07:28:05 -0600

    Convertir el formulario normal, en ngForm, creamos un envento sumbit de angular (ngSubmit), pusimos indentitificador #formulario, creamos un objeto para guardar los datos del formulario, preparamos cada input para guardar ese dato en el objeto

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms'; // formulario de angular
 
 @Component({
   selector: 'app-template',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class TemplateComponent implements OnInit {
+  usuario: object =
+  {
+      nombre: 'Joaquin',
+      apellido: 'Guzman',
+      correo: 'emoxito_chapito_666@hotmail.com'
+  } // end object usuario
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  enviarDatos( formulario: NgForm )
+  {
+    console.log("ngForm", formulario);
+    console.log("Valor", formulario.value);
+
+    console.log("Usuario", this.usuario);
+  } // end guardarDatos
 }
